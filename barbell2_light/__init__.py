c18c0482ac2f8408940c23523f3fb707be3db946
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-03-13 08:33:09 +0100

    Saving version 0.3.0 before deploying to PyPI. Made some methods and Logger in utils available at root package

--- barbell2_light/__init__.py
+++ barbell2_light/__init__.py
@@ -2,6 +2,12 @@
 
 __author__ = """Ralph Brecheisen"""
 __email__ = 'ralph.brecheisen@gmail.com'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 from .castorclient import CastorClient
+from .utils import Logger
+from .utils import current_time_millis
+from .utils import current_time_secs
+from .utils import elapsed_millis
+from .utils import elapsed_secs
+from .utils import duration
