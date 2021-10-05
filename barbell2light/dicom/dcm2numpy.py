421e815b7df8ba3cc32022e1fa1664fd3ffd441e
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-10-05 11:55:40 +0200

    Saving version 0.23.0 before deploying to PyPI.

---
+++ barbell2light/dicom/dcm2numpy.py
@@ -0,0 +1,43 @@
+import pydicom
+
+import numpy as np
+
+
+class Dcm2Numpy(object):
+
+    def __init__(self):
+        self._input_dicom_file_path = None
+        self._output_numpy_array = None
+        self._normalize_enabled = False
+        self._shape = None
+
+    def set_input_dicom_file_path(self, input_dicom_file_path):
+        self._input_dicom_file_path = input_dicom_file_path
+
+    def set_normalize_enabled(self, enabled=True):
+        self._normalize_enabled = enabled
+
+    def get_output_numpy_array(self):
+        return self._output_numpy_array
+
+    def get_shape(self):
+        return self._shape
+
+    def execute(self):
+        if self._input_dicom_file_path is None:
+            raise RuntimeError('Input DICOM file path not set')
+        p = pydicom.read_file(self._input_dicom_file_path)
+        self._shape = (p.Rows, p.Columns)
+        self._output_numpy_array = p.pixel_array.reshape(self._shape)
+        if self._normalize_enabled:
+            b = p.RescaleIntercept
+            m = p.RescaleSlope
+            self._output_numpy_array = m * self._output_numpy_array + b
+        # print('{}, {}'.format(np.min(self._output_numpy_array), np.max(self._output_numpy_array)))
+
+
+if __name__ == '__main__':
+    component = Dcm2Numpy()
+    component.set_input_dicom_file_path('/Users/Ralph/data/Jorne/P0001.dcm')
+    component.execute()
+    print(component.get_output_numpy_array().shape)
